d5eb96b60ea7dccb4bebf1160c4b36fff5e47b67
khaled sukkar <khlaed65241@gmail.com>
2024-02-29 14:43:52 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,3 +1,4 @@
+import logging
 from django.contrib.auth import get_user_model
 from django import template
 from django.utils.html import escape, format_html
@@ -6,6 +7,7 @@ from blog.models import Post
 
 user_model = get_user_model()
 register = template.Library()
+logger = logging.getLogger(__name__)
 
 @register.filter
 def author_details(author):
@@ -72,4 +74,5 @@ def author_details_tag(context):
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
   posts = Post.objects.exclude(pk=post.pk)[:5]
+  logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
   return {"title": "Recent Posts", "posts":posts}
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,19 @@
 import logging
 from django.shortcuts import render, get_object_or_404
-from blog.models import Post
 from django.utils import timezone
 from django.shortcuts import redirect
 from blog.forms import CommentForm
+from blog.models import Post
+
 
 logger = logging.getLogger(__name__)
 # Create your views here.
+
+
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.debug("Got %d posts", len(posts))
-    return render(request,
-     "blog/index.html",
-     {"posts":posts})
-
+    return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
