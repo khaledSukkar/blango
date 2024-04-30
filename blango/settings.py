d832f1d48d5cb2470e49060ed243fd9fc94f8127
khaled sukkar <khlaed65241@gmail.com>
2024-04-30 08:56:26 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -92,6 +92,14 @@ class Dev(Configuration):
       },
   ]
 
+  REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
+
   WSGI_APPLICATION = 'blango.wsgi.application'
 
 
