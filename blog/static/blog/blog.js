0289c5893cb48f047c02e5f63f25d370d4b212db
khaled sukkar <khlaed65241@gmail.com>
2024-10-17 14:45:55 +0000

    Finish JavaScript promises

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,9 +1,25 @@
-const theNumber = 1
-let yourName = 'Ben'
+const lazyAdd = function(a,b) {
+  const doAdd = (resolve, reject) => {
+    if(typeof a !== "number" || typeof b !== "number")
+    {
+      reject("a and b must be numbers");
+    } else {
+      const sum = a + b;
+      resolve(sum)
+    }
 
-if (theNumber === 1) {
-  let yourName = 'Leo'
-  alert(yourName)
+  }
+  return new Promise(doAdd);
 }
 
-alert(yourName)
+function resolvedCallback(data) {
+  console.log("resolved data is:  " + data);
+}
+
+function rejectedCallback(message) {
+  console.log('rejected with message: ' + message);
+}
+
+
+const p = lazyAdd(5,6);
+p.then(resolvedCallback, rejectedCallback)
