f17d7c4b598941c915fdccaf528e300a6d8a1533
khaled sukkar <khlaed65241@gmail.com>
2024-10-17 15:07:55 +0000

    Finish ReactJS

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,25 +1,35 @@
-const lazyAdd = function(a,b) {
-  const doAdd = (resolve, reject) => {
-    if(typeof a !== "number" || typeof b !== "number")
-    {
-      reject("a and b must be numbers");
-    } else {
-      const sum = a + b;
-      resolve(sum)
-    }
-
+class ClickButton extends React.Component {
+  state = {
+    wasClicked: false;
   }
-  return new Promise(doAdd);
+
+  handleClick() {
+    this.setState(
+      {
+        wasClicked : true;
+      }
+    )
+  }
+
+  render() {
+    let buttonText;
+
+    if(this.state.wasClicked)
+    {
+      buttonText = "clicked";
+    } else {
+      buttonText = "Click me";
+    }
+  }
+
+  return React.CreateElement(
+    'button',
+    {
+      className :'btn btn-primary mt-2',
+      onClick() => {
+        this.handleClick()
+      }
+    },
+    buttonText
+  )
 }
-
-function resolvedCallback(data) {
-  console.log("resolved data is:  " + data);
-}
-
-function rejectedCallback(message) {
-  console.log('rejected with message: ' + message);
-}
-
-
-const p = lazyAdd(5,6);
-p.then(resolvedCallback, rejectedCallback)
